505efd95e0fe83b3f769fa05ec3a418e6d151afb
Gabriel Martinez <gabriel.martinez.reyes@gmail.com>
2017-11-14 12:49:55 +0100

    Trafficlights

---
+++ TrafficLights/TrafficLights/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TrafficLights
+//
+//  Created by Gabriel Martinez on 2017-11-14.
+//  Copyright © 2017 Gabriel Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var trafficImage: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+// Test
